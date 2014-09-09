1d701a625645282a4f5f1596e8adab4f904ceef6
Allan Chappell <generalredneck@gmail.com>
2014-09-09 12:49:04 -0500

    Found how to get configuration. Implemented.

--- src/Drupal/Composer/DrupalLIbraryInstaller.php
+++ src/Drupal/Composer/DrupalLIbraryInstaller.php
@@ -4,17 +4,38 @@ namespace Drupal\Composer;
 
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
+use Composer\IO\IOInterface;
+use Composer\Composer;
 
 class DrupalLibraryInstaller extends LibraryInstaller
 {
+    public function __construct(IOInterface $io, Composer $composer)
+    {
+        parent::__construct($io, $composer);
+        $extra = $this->composer->getPackage()->getExtra();
+        $this->drupalConfig = isset($extra['drupal-libraries']) ? $extra['drupal-libraries'] : array();
+        $this->drupalLibrariesPath = isset($this->drupalConfig['library-directory']) ? $this->drupalConfig['library-directory'] : 'www/sites/all/libraries/';
+        if (substr($this->drupalLibrariesPath, -1) != '/') {
+          $this->drupalLibrariesPath .= '/';
+        }
+        $this->drupalLibraries = isset($this->drupalConfig['libraries']) ? $this->drupalConfig['libraries'] : array();
+        $this->drupalLibraryMap = array();
+        foreach ($this->drupalLibraries as $library) {
+          $this->drupalLibraryMap[$library['package']] = $library['name'];
+        }
+    }
     /**
      * {@inheritDoc}
      */
     public function getPackageBasePath(PackageInterface $package)
     {
-        //var_dump($this->composer->getConfig());
-        var_dump($this->composer->getExtra());
-        return parent::getPackageBasePath($package);
+      if (empty($this->drupalLibraryMap[$package->getPrettyName()])) {
+        $path = parent::getPackageBasePath($package);
+      }
+      else {
+        $path = $this->drupalLibrariesPath . $this->drupalLibraryMap[$package->getPrettyName()];
+      }
+      return $path;
     }
 
 }
