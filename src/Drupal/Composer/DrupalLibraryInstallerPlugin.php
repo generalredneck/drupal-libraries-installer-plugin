d8fc058bac6c8cd018bafdde9af0fceefb0be0be
Chris Green <chrisgreen@email.arizona.edu>
2020-10-27 16:44:11 -0700

    Support Composer v2

--- src/Drupal/Composer/DrupalLibraryInstallerPlugin.php
+++ src/Drupal/Composer/DrupalLibraryInstallerPlugin.php
@@ -8,9 +8,27 @@ use Composer\Plugin\PluginInterface;
 
 class DrupalLibraryInstallerPlugin implements PluginInterface
 {
+    /**
+     * {@inheritDoc}
+     */
     public function activate(Composer $composer, IOInterface $io)
     {
         $installer = new DrupalLibraryInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
 }
